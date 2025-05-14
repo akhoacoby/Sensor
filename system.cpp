0eaef97110b29b734d21957029edb979e994b4d0
Mm-12324 <malakbahda11@gmail.com>
2025-05-14 17:09:50 +0200

    Update system.cpp

--- system.cpp
+++ system.cpp
@@ -1,19 +1,49 @@
 using namespace std;
 #include <iostream>
 #include <ctime>
-//------------------------------------------------------ Include personnel
 #include "System.h"
 
+//----------------------------------------------------- Constructeur
+System::System()
+{
+    
+}
 
+//----------------------------------------------------- Destructeur
+System::~System()
+{
 
-//-------------------------------------------- Constructeurs - destructeur
-System::System ( const System & unSystem )
+}
 
+//----------------------------------------------------- Méthodes
 
-System::System ( )
+void System::loadData()
+{
+    //  Chargement des données
+}
 
+bool System::analyzeSensor(const std::string & sensorId)
+{
+    //  Analyse de capteur
+}
 
-System::~System ( )
+double System::calculateMeanInArea(double lat, double lon, double rad,const std::string & start, const std::string & end,std::string attributeId)
+{
+    // calcul de moyenne
+}
+
+std::vector<pair<Sensor, int>> System::rankSimilarSensors(const std::string & sensorId,const std::string & start,
+const std::string & end,const std::string & timestamp, std::string attributeId)
+{
+    //  classement des capteurs
+    return;
+}
+
+bool System::evaluateSensorReliability(const std::string & sensorId)
+{
+    // évaluation de fiabilité
+}
+
 
 
 //------------------------------------------------------------------ PRIVE
