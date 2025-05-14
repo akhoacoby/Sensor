ef0ec8ca021a97290d278621990a1c3b21bbc5cf
Bartosz Bagiński <bartosz.baginski@insa-lyon.fr>
2025-05-14 15:48:19 +0200

    Fix merge problems

--- Cleaner.h
+++ Cleaner.h
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 /**
  * @name AirCleaner.h
  * @date 2025.05.14
@@ -7,25 +6,10 @@
 
 #ifndef _AIRCLEANER_H
 #define _AIRCLEANER_H
-=======
-/*************************************************************************
-                           Cleaner  -  description
-                             -------------------
-    début                : $DATE$
-    copyright            : (C) $YEAR$ par $AUTHOR$
-    e-mail               : $EMAIL$
-*************************************************************************/
-
-//---------- Interface de la classe <Cleaner> (fichier Cleaner.h) ----------------
-#if ! defined ( Cleaner_H )
-#define Cleaner_H
->>>>>>> a53597cc86747c1d1e3e4fdc984b66ae8ea20064
-
 #include <string>
 
 #include "Time.h"
 
-<<<<<<< HEAD
 class AirCleaner
 {
   private:
@@ -35,69 +19,3 @@ class AirCleaner
     Time startTime;
     Time stopTime;
 }
-=======
-//------------------------------------------------------------------ Types
-
-//------------------------------------------------------------------------
-// Rôle de la classe <Cleaner>
-//
-//
-//------------------------------------------------------------------------
-
-class Cleaner : public Cleaner
-{
-//----------------------------------------------------------------- PUBLIC
-
-public:
-//----------------------------------------------------- Méthodes publiques
-    // type Méthode ( liste des paramètres );
-    // Mode d'emploi :
-    //
-    // Contrat :
-    //
-
-
-//------------------------------------------------- Surcharge d'opérateurs
- 
-    // Mode d'emploi :
-    //
-    // Contrat :
-    //
-
-
-//-------------------------------------------- Constructeurs - destructeur
-    Cleaner ( const Cleaner & unCleaner );
-    // Mode d'emploi (constructeur de copie) :
-    //
-    // Contrat :
-    //
-
-    Cleaner ( );
-    // Mode d'emploi :
-    //
-    // Contrat :
-    //
-
-    virtual ~Cleaner ( );
-    // Mode d'emploi :
-    //
-    // Contrat :
-    //
-
-//------------------------------------------------------------------ PRIVE
-
-private:
-string cleanerId;
-double latitude;
-double longitude;
-time_t startTime;
-time_t stopTime;
-
-
-};
-
-//-------------------------------- Autres définitions dépendantes de <Cleaner>
-
-#endif // Cleaner_H
-
->>>>>>> a53597cc86747c1d1e3e4fdc984b66ae8ea20064
