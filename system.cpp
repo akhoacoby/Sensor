51501b417c72acfc9cd5fdd627ece24326a1a143
Mm-12324 <malakbahda11@gmail.com>
2025-05-14 15:55:43 +0200

    Update system.cpp

--- system.cpp
+++ system.cpp
@@ -16,55 +16,16 @@ using namespace std;
 //------------------------------------------------------ Include personnel
 #include "System.h"
 
-//------------------------------------------------------------- Constantes
-
-//----------------------------------------------------------------- PUBLIC
-
-//----------------------------------------------------- Méthodes publiques
-// type System::Méthode ( liste des paramètres )
-// Algorithme :
-//
-//{
-//} //----- Fin de Méthode
-
-
-//------------------------------------------------- Surcharge d'opérateurs
-System & System::operator = ( const System & unSystem )
-// Algorithme :
-//
-{
-} //----- Fin de operator =
 
 
 //-------------------------------------------- Constructeurs - destructeur
 System::System ( const System & unSystem )
-// Algorithme :
-//
-{
-#ifdef MAP
-    cout << "Appel au constructeur de copie de <System>" << endl;
-#endif
-} //----- Fin de System (constructeur de copie)
 
 
 System::System ( )
-// Algorithme :
-//
-{
-#ifdef MAP
-    cout << "Appel au constructeur de <System>" << endl;
-#endif
-} //----- Fin de System
 
 
 System::~System ( )
-// Algorithme :
-//
-{
-#ifdef MAP
-    cout << "Appel au destructeur de <System>" << endl;
-#endif
-} //----- Fin de ~System
 
 
 //------------------------------------------------------------------ PRIVE
