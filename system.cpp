a22109d5013ed3b874823510d8f4f7814e2ab6f9
Mm-12324 <malakbahda11@gmail.com>
2025-05-30 17:30:17 +0200

    Update system.cpp

--- system.cpp
+++ system.cpp
@@ -47,9 +47,30 @@ bool System::analyzeSensor(const std::string & sensorId)
     //  Analyse de capteur
 }
 
-double System::calculateMeanInArea(double lat, double lon, double rad,const std::string & start, const std::string & end,std::string attributeId)
+double System::calculateMeanInArea(double lat, double lon, double rad,const std::string & start,
+const std::string & end,std::string attributeId)
 {
-    // calcul de moyenne
+        Time tStart(start);
+    Time tEnd(end);
+    double total = 0.0;
+    int count = 0;
+
+    for (const Sensor& s : list_sensors) {
+        double dist = sqrt(pow(s.get_lat() - lat, 2) + pow(s.get_long() - lon, 2));
+        if (dist <= rad) {
+            vector<Measurement> sensorMeasurements = s.get_measurements(list_measurements, tStart);
+            for (const Measurement& m : sensorMeasurements) {
+                if (m.getTimeStamp() <= tEnd && m.getAttribute_id() == attributeId) {
+                    total += m.getValue();
+                    count++;
+                }
+            }
+        }
+    }
+
+    if (count == 0) return -1;
+    return total / count;
+
 }
 
 std::vector<pair<Sensor, int>> System::rankSimilarSensors(const std::string & sensorId,const std::string & start,
